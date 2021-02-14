6910cec182796751e443dd255ed1a0972147a02e
Mahesh Babu Gorantla <maheshbabugorantla@gmail.com>
2021-02-14 08:50:31 -0600

    Added entrypoint scripts for Bash & ZSH Shells

---
+++ services/stock_league/run-flask.sh
@@ -0,0 +1,5 @@
+#!/bin/zsh
+
+export FLASK_APP="stock_league.py"
+export FLASK_ENV="development"
+python /usr/src/app/manage.py run -h 0.0.0.0
---
+++ services/stock_league/run-flask.zsh
@@ -0,0 +1,5 @@
+#!/bin/zsh
+
+export FLASK_APP="stock_league.py"
+export FLASK_ENV="development"
+flask run
